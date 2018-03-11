ff40fc044646919fd7c1d63cea1f421043f57823
Yuukiii <garfield.heron@gmail.com>
2018-03-10 20:30:04 -0500

    changed to use ip

--- examples/server.js
+++ examples/server.js
@@ -4,12 +4,12 @@ const RxAmqpLib = require('../');
 
 const config = {
   queue: 'test_queue',
-  host: 'amqp://localhost'
+  host: 'amqp://127.0.0.1'
 };
 
 // Process stream
 RxAmqpLib.newConnection(config.host)
   .flatMap(connection => connection.createChannel())
   .flatMap(channel => channel.assertQueue(config.queue, { durable: false }))
-  .flatMap(reply => reply.channel.consume(config.queue, { noAck: true }))
+  .flatMap(reply => reply.channel.consume(config.queue, { noAck: false }))
   .subscribe(message => console.log(message.content.toString()));
